cca44381d5d5d20a1f27878093fe72fe3e0fb161
Luana Marques <luanamarques.silv@gmail.com>
2025-04-03 23:10:18 -0300

    Inicialização do projeto OXEFOOD

---
+++ src/main/java/br/com/ifpe/oxefood_api_luana/OxefoodApiLuanaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_luana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiLuanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiLuanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefood_api_luana/OxefoodApiLuanaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_luana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiLuanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
