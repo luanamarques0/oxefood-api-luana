8c6565648c4c8fc9ca1bd35825c8cf2b92c26cf2
luanamarques0 <luanamarques.silv@gmail.com>
2025-07-02 22:38:55 -0300

    feat:Adicionar regra de négocio para validar se o produto tem um valor maior que 20 e menor que 100

--- src/main/java/br/com/ifpe/oxefood_api_luana/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood_api_luana/modelo/produto/ProdutoService.java
@@ -5,16 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood_api_luana.util.exception.ProdutoException;
 import jakarta.transaction.Transactional;
 
 @Service
 public class ProdutoService {
-    
+
     @Autowired
     private ProdutoRepository repository;
 
     @Transactional
     public Produto save(Produto produto) {
+
+        if ((produto.getValorUnitario() < 20) && (produto.getValorUnitario() > 100)) {
+            throw new ProdutoException(ProdutoException.MSG_VALOR_MINIMO_E_MAXIMO_PRODUTO);
+        }
+
         produto.setHabilitado(Boolean.TRUE);
         return repository.save(produto);
     }
